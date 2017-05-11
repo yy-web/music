bdb68715b349697d5a47b364fefcf876eb9fcc77
yy <403086967@qq.com>
2017-05-11 22:43:12 +0800

    0511-

--- src/test/main.js
+++ src/test/main.js
@@ -19,12 +19,49 @@ class Main extends React.Component {
                                     <p className="name ell">123</p>
                                     <p className="copywriter ell">1321323</p>
                                 </div>
-
                                 <span className="myFont icon_googleplay play"></span>
                             </div>
-
                         </div>
                     </div>
+                    <div className="m_list_item">
+                        <img src="http://p1.music.126.net/ZSaf93fTR1OC--umy-dpvg==/18714787418286872.jpg" alt="recommend_m_list"/>
+                        <div className="item_info">
+                            <div className="infoBox">
+                                <div style={{width: '100%'}}>
+                                    <p className="name ell">123</p>
+                                    <p className="copywriter ell">1321323</p>
+                                </div>
+                                <span className="myFont icon_googleplay play"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="m_list_item">
+                        <img src="http://p1.music.126.net/ZSaf93fTR1OC--umy-dpvg==/18714787418286872.jpg" alt="recommend_m_list"/>
+                        <div className="item_info">
+                            <div className="infoBox">
+                                <div style={{width: '100%'}}>
+                                    <p className="name ell">123</p>
+                                    <p className="copywriter ell">1321323</p>
+                                </div>
+                                <span className="myFont icon_googleplay play"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <TitleBox title="推荐歌曲" more={true}/>
+                <div className="recommend_s_list">
+                    <ul>
+                        <li>
+                          <span className="song_name">听海</span>
+                          <span className="artists_name">张惠妹</span>
+                          <span className="myFont icon_play"></span>
+                          <span className="myFont icon_download"></span>
+                        </li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         )
